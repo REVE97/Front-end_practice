49f691a51c1176c335fd7251248e95ee3854dc6f
zxcv9675 <zxcv9675@naver.com>
2024-04-20 15:40:20 +0900

    수의 표현, 문자 표현, 변수 사용법

---
+++ JavaScript/script_1.js
@@ -0,0 +1,40 @@
+
+/*
+
+// 수의 표현과 연산
+
+alert(1);
+console.log(1);
+Math.pow(3,2); // 3의 제곱
+Math.round(10.6); // round : 반올림
+Math.ceil(10.2); // 가까운 정수보다 큰 정수
+Math.floor(10.2); // 가까운 정수보다 작은 정수
+Math.sqrt(9); // 9의 제곱근
+Math.random(); // 랜덤값
+Math.round(100*Math.random()); // 100보다 작은 랜덤값 정수
+
+*/
+
+
+// 문자 표현
+
+//window.alert();   // window 객체의 메소드를 사용하여 alert 출력
+
+window.console.log(1);
+console.log(1);
+
+console.log('egoing \'s coding everybody');  // ' ' or " " 사이에 ' or " 를 넣고 싶을 때는 \ 를 추가해서 출력
+
+console.log(typeof 1); // typeof 를 통해서 문자의 타입을 확인할 수 있음
+
+console.log("coding everybody".length);  // length 메소드를 통해서 문자의 길이를 알 수 있음 
+
+console.log("code".indexOf("c")); // indexOf 메소드를 통해서 문자가 존재하는 인덱스를 알 수 있음
+
+// 변수 사용법
+
+let a = 1; 
+console.log("a : " + a);
+
+a=2;
+console.log("a : " + a);
