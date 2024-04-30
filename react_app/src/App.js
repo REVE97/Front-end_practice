6d2dc2ab798d94d2474a55f74f50566f919cc5a3
zxcv9675 <zxcv9675@naver.com>
2024-04-30 14:00:56 +0900

    React Practice

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,45 +2,57 @@
 // App 실행 : npm start
 // 서버 Build : npm run build
 // 서버 배포 : npx serve -s build
-// 작업 중지 : crtl + c 
+// 작업 중지 : crtl + c
+
+// props : 속성
+// 함수 매개변수 값에 props 를 추가해서 사용
+// 태그에 속성을 추가하여 함수에 적용함
 
 import logo from './logo.svg';
 import './App.css';
 
-function Header(){
+function Header(props){
   return <header>
-  <h1><a href="/">WEB</a></h1>
+  <h1><a href="/">{props.title}</a></h1>
 </header>
 }
 
-function Nav(){
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+  }
   return <nav>
   <ol>
-    <li><a href="/read/1">html</a></li>
-    <li><a href="/read/2">css</a></li>
-    <li><a href="/read/3">js</a></li>
+    {lis}
   </ol>
 </nav>
 }
 
-function Article(){
+function Article(props){
   return <article>
-  <h2>Welcome</h2>
-  Hello, WEB
+  <h2>{props.title}</h2>
+  {props.body}
 </article>
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'html is ...'},
+    {id:3, title:'javascript', body:'html is ...'},
+  ]
   return (
     <div className="App">
       
       
       
-      <Header></Header>
+      <Header title="REACT"></Header>
 
-      <Nav></Nav>
+      <Nav topics={topics}></Nav>
 
-      <Article></Article>
+      <Article title="Welcome" body="Hello, WEB"></Article>
 
     </div>
   );
