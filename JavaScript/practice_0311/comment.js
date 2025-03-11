1f8ff3c882f3d8f8618a4453677b594514445478
zxcv9675 <zxcv9675@naver.com>
2025-03-11 17:42:33 +0900

    구조 분헤, 전개, 객체 병합 연습

---
+++ JavaScript/practice_0311/comment.js
@@ -0,0 +1,25 @@
+//구조 분해, 전개, 객체 병합 실습
+const personalInfo = {
+    name: '이효석',
+    age: `Don't ask this :)`,
+    email: 'xenosign@naver.com'
+};
+
+const jobInfo = {
+    position: '코딩 강사',
+    experience: '?년'
+};
+
+//병합
+const profile = {...personalInfo, ...jobInfo, addr: '서대문구'};
+
+
+//구조 분해
+const{name, age, email, position, experience, addr} = profile;
+
+console.log('이름: ',name);
+console.log('나이: ',age);
+console.log('이메일: ',email);
+console.log('직책: ',position);
+console.log('경력: ',experience);
+console.log('지역: ',addr);
