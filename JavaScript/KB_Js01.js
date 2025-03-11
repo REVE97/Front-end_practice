583794fc5fe990ec83210e5a123b1615fd1303d3
zxcv9675 <zxcv9675@naver.com>
2025-03-11 17:05:15 +0900

    부트캠프 Day3(JS 기본 문법)

---
+++ JavaScript/KB_Js01.js
@@ -0,0 +1,16 @@
+//2025.3.10
+//통합강의: 9장 자바스크립트 기본 문법
+
+let array= [1, 10, 20, , 30, ,60];
+
+for(let i=0; i<array.length; i++){
+    console.log(array[i]);
+}
+
+for(let i in array){
+    console.log(i, array[i]);
+}
+
+for(let i of array) {
+    console.log(i);
+}
