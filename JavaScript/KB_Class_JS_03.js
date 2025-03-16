e7b8d02af565d91141414b6df278452600b7af54
zxcv9675 <zxcv9675@naver.com>
2025-03-16 14:41:04 +0900

    부트캠프 Day5 (모듈)

---
+++ JavaScript/KB_Class_JS_03.js
@@ -0,0 +1,16 @@
+// Day5 2025.3.12
+// 통합강의: Node.js 1,2장
+
+//1. npm init 오류 날때 
+//1) 정책 변경
+//Get-ExecutionPolicy 실행 = Restricted 출력
+//Set-ExecutionPolicy 실행 ReomoteSigned 실행
+//npm init 실행
+
+//2)cmd 창을 열어서 npm init, npm install ~, npm uninstall 실행
+
+//2. node_module 옮길때는 package.json만 옮겨서 npm install 실행
+
+//3. 동기 vs 비동기
+//4. 모듈 선언 및 사용 => module.exports, require()
+//5. 모듈 내보내기 : export 가져오기: import 변수 from 모듈 파일
