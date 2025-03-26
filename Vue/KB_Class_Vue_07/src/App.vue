d7c58b995f8c8b59d44e9b155448ddbf1c609e87
zxcv9675 <zxcv9675@naver.com>
2025-03-26 13:03:53 +0900

    부트캠프 Day13 통합강의 (Vue 컴포넌트 심화)

---
+++ Vue/KB_Class_Vue_07/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import Child1  from './components/Child1.vue';
+  import Child2 from './components/Child2.vue';
+  import Child3 from './components/Child3.vue';
+  import NoSlotTest from './components/NoSlotTest.vue';
+  import SlotTest from './components/SlotTest.vue';
+</script>
+
+<template>
+  <div>
+    <Child1 />
+    <Child2 />
+    <Child3 />
+
+    <hr />
+
+    <NoSlotTest />
+
+    <hr />
+
+    <SlotTest />
+  </div>
+</template>
+
+<script>
+  export default { 
+    name: 'App',
+};
+</script>
+
+<style scoped>
+
+</style>
