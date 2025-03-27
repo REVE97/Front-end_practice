287f007c83570fb35d1c19e2d5696a1355e0d978
zxcv9675 <zxcv9675@naver.com>
2025-03-27 12:59:03 +0900

    부트캠프 Day14 통합강의 (Composition API)

---
+++ Vue/KB_Class_Vue_08/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Calc from './components/Calc.vue';
+</script>
+
+<template>
+  <div>
+    <Calc />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+  }
+</script>
+
+<style scoped>
+
+</style>
