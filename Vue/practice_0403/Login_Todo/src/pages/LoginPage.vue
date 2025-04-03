b9f30782aa8c43d9cd0188808e2afb9ee90410f5
zxcv9675 <zxcv9675@naver.com>
2025-04-03 13:24:15 +0900

    부트캠프 Day19 오후강의 (Json server, Axios를 활용한 로그인 투두리스트)

---
+++ Vue/practice_0403/Login_Todo/src/pages/LoginPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1>Login page</h1>
+    아이디 : <input type="text" v-model.trim="userId" /> 
+    <br />
+    패스워드 : <input type="password" v-model.trim="userPassword" /> 
+    <br />
+    <button v-on:click="login">로그인</button> / <button>로그아웃</button>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const userId = ref('');
+const userPassword = ref('');
+</script>
+
+<style>
+
+</style>
---
+++ Vue/practice_0403/Login_Todo/src/pages/TodoPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <h1>Todo Page</h1>
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
